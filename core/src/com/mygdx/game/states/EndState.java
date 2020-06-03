7aecd122a1b45859ff0306f740584e1f7242c26c
Yelyzaveta Boiarchuk <liz.bk06@gmail.com>
2020-06-03 14:30:55 +0300

    качацй это

--- core/src/com/mygdx/game/states/EndState.java
+++ core/src/com/mygdx/game/states/EndState.java
@@ -11,14 +11,14 @@ public class EndState extends State {
 
     EndState(GameStateManager gsm, long gs, long ms) {
         super(gsm);
-        gameScore = gs;
+     /*   gameScore = gs;
         moneyScore = ms;
         gsm.currentUser.money+=ms;
         System.out.println("money at all");
         if (gsm.currentUser.score<gameScore) {
             gsm.currentUser.score=gameScore;
             System.out.println("new best score");
-        }
+        }*/
     }
 
 
