7f3aba117f95347e51ba33555d6708e68a6c9189
Mariia Vareshchuk <maryvaresh12@gmail.com>
2020-06-04 12:48:30 +0300

    trying to push all assets

--- core/src/com/mygdx/game/states/AuthorizationState.java
+++ core/src/com/mygdx/game/states/AuthorizationState.java
@@ -12,6 +12,7 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.mygdx.game.StartClass;
+import jdk.vm.ci.code.CodeUtil;
 
 public class AuthorizationState extends State {
 
@@ -19,7 +20,7 @@ public class AuthorizationState extends State {
     public Texture grass;
     public Texture sittingLama;
     private Texture loginButton;
-    private Texture loginButtonPressed;
+    private Texture registrationButton;
     private FreeTypeFontGenerator fontGenerator;
     private FreeTypeFontGenerator.FreeTypeFontParameter fontParameter;
     private FreeTypeFontGenerator.FreeTypeFontParameter fontParameter2;
@@ -30,7 +31,7 @@ public class AuthorizationState extends State {
     private TextField usernameTextField;
     private TextField passwordTextField;
     private Skin defaultSkin;
-    private Stage stage;
+  //  private Stage stage;
 
     public AuthorizationState(GameStateManager gsm) {
         super(gsm);
@@ -42,7 +43,7 @@ public class AuthorizationState extends State {
         grass = new Texture("grass.JPEG");
         sittingLama = new Texture("sittingLama.png");
         loginButton = new Texture("uiskins/loginButton.png");
-        loginButtonPressed = new Texture("uiskins/loginButtonPressed.png");
+        registrationButton = new Texture("uiskins/loginButton.png");
         defaultSkin = new Skin(Gdx.files.internal("skins/uiskin.json"));
         fontGenerator = new FreeTypeFontGenerator(Gdx.files.internal("Modulus-Bold.otf"));
         fontParameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
@@ -57,15 +58,37 @@ public class AuthorizationState extends State {
         loginFontParameter.size = 30;
         loginFontParameter.color= Color.WHITE;
         loginFont = fontGenerator.generateFont(loginFontParameter);
-        stage = new Stage();
-        Gdx.input.setInputProcessor(stage);
+    //    stage = new Stage();
+     //   Gdx.input.setInputProcessor(stage);
         usernameTextField = new TextField("", defaultSkin);
         passwordTextField = new TextField("", defaultSkin);
     }
 
     @Override
     public void handleInput() {
+        if(Gdx.input.getX() > 185 &&
+                Gdx.input.getX() < 320 &&
+                Gdx.input.getY() > 405 &&
+                Gdx.input.getY() < 438) {
+            if (Gdx.input.isTouched()) {
+                this.dispose();
+                gsm.set(new MenuState(gsm));
+            }
+        }
+        if(Gdx.input.getX() > 185 &&
+                Gdx.input.getX() < 320 &&
+                Gdx.input.getY() > 465 &&
+                Gdx.input.getY() < 498) {
+            if (Gdx.input.isTouched()) {
+                this.dispose();
+                gsm.set(new RegistrationState(gsm));
+            }
+        }
 
+        if(Gdx.input.isKeyPressed(Input.Keys.R)){
+            this.dispose();
+            gsm.set(new RegistrationState(gsm));
+        }
     }
 
     @Override
@@ -80,26 +103,20 @@ public class AuthorizationState extends State {
         sb.draw(grass, 0, -50);
         sb.draw(sittingLama, StartClass.WIDTH/2-sittingLama.getWidth()/2, 70);
         sb.draw(loginButton, StartClass.WIDTH/2-loginButton.getWidth()/2, StartClass.HEIGHT/2-100);
+        sb.draw(registrationButton, StartClass.WIDTH/2-loginButton.getWidth()/2, StartClass.HEIGHT/2-160);
         font.draw(sb, "Welcome to Jumping Lama!", 35, StartClass.HEIGHT-50);
-        font2.draw(sb, "Enter your username", 125, StartClass.HEIGHT/2+120);
+        font2.draw(sb, "Enter your login", 125, StartClass.HEIGHT/2+120);
         font2.draw(sb, "Enter your password", 125, StartClass.HEIGHT/2+40);
         loginFont.draw(sb, "Login", StartClass.WIDTH/2-33, StartClass.HEIGHT/2-60);
+        loginFont.draw(sb, "Sign up", StartClass.WIDTH/2-42, StartClass.HEIGHT/2-120);
         usernameTextField.setPosition(StartClass.WIDTH/2, StartClass.HEIGHT/2+60,100);
         usernameTextField.setSize(250, 25);
         usernameTextField.draw(sb, 60);
         passwordTextField.setPosition(StartClass.WIDTH/2, StartClass.HEIGHT/2-15,100);
         passwordTextField.setSize(250, 25);
         passwordTextField.draw(sb, 60);
-        stage.act();
-        if(Gdx.input.getX() > 185 &&
-        Gdx.input.getX() < 320 &&
-        Gdx.input.getY() > 405 &&
-        Gdx.input.getY() < 438) {
-            if (Gdx.input.isTouched()) {
-                this.dispose();
-                gsm.set(new MenuState(gsm));
-            }
-        }
+     //   stage.act();
+
         sb.end();
     }
 
@@ -111,6 +128,9 @@ public class AuthorizationState extends State {
         usernameTextField.cut();
         passwordTextField.cut();
         fontGenerator.dispose();
+        registrationButton.dispose();
+        loginButton.dispose();
     }
+
 
 }
